dfd48e4c38b517dd6f4cbd2e82bd73915a11e74a
jokudev <git@joku.dev>
2024-05-22 11:06:09 +0200

    add environments and supabase vars

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  supabaseUrl: 'https://sdalanqdwdvtuwqztrdq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYWxhbnFkd2R2dHV3cXp0cmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3Njk5MTYsImV4cCI6MjAzMTM0NTkxNn0.y7jcrEdVT7_8UNSu90parzhI06OMmb4TTzQDQG8gTH0',
+  supabaseRedirect: 'http://localhost:4200/signin',
+};
+
