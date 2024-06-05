17a21dceb0a5e6c8c4acc552437699ac1c09171b
simon <afs@v>
2024-06-05 10:11:00 +0200

    vehicle service

---
+++ src/app/services/vehicle.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, importProvidersFrom } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Vehicle } from '../data/vehicle';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class VehicleService {
+    
+    readonly backendUrl = 'cars';
+    
+
+    constructor(private http: HttpClient) {
+    }
+  
+    public getList(): Observable<Vehicle[]> {
+      return this.http.get<Vehicle[]>(environment.apiUrl + this.backendUrl);
+    }
+  
+    public getOne(id: number): Observable<Vehicle> {
+      return this.http.get<Vehicle>(environment.apiUrl + this.backendUrl + `/${id}`);
+    }
+  
+    public update(cars: Vehicle): Observable<Vehicle> {
+      return this.http.put<Vehicle>(environment.apiUrl + this.backendUrl + `/${cars.id}`, cars);
+    }
+  
+    public save(cars: Vehicle): Observable<Vehicle> {
+      return this.http.post<Vehicle>(environment.apiUrl + this.backendUrl, cars);
+    }
+  
+    public delete(id: number): Observable<HttpResponse<string>> {
+      return this.http.delete<string>(environment.apiUrl + this.backendUrl + `/${id}`, {observe: 'response'});
+    }
+  }
+
+
+  
