9673340f38eb1cc688c2bfb559c624371f970de3
jokudev <git@joku.dev>
2024-06-05 13:07:58 +0200

    change auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../data/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,16 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class AuthService {
   backendUrl = environment.apiUrl + '/api/login';
 
-  private isAdminBehaviorSubject = new BehaviorSubject<boolean | undefined>(false);
-  public isAdmin = this.isAdminBehaviorSubject.asObservable();
+  private userBehaviorSubject = new BehaviorSubject<User | null | undefined>(null);
+  public user = this.userBehaviorSubject.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   async login(username: string, password: string) {
     await this.http.post(this.backendUrl, { username, password }).subscribe((res: any) => {
       if (res.message == 'Login successful') {
-        this.isAdminBehaviorSubject.next(res.isAdmin);
+        this.userBehaviorSubject.next(res.user);
+        this.router.navigate(['/']);
       }
     });
   }
