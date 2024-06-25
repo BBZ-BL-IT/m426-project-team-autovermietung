7f54563a5744e1f3827d2dace89d79f4174c7c78
jokudev <git@joku.dev>
2024-06-25 08:44:33 +0200

    make user persistent

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { User } from '../data/user';
 import { Router } from '@angular/router';
-
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +11,29 @@ import { Router } from '@angular/router';
 export class AuthService {
   backendUrl = environment.apiUrl + '/api/login';
 
-  private userBehaviorSubject = new BehaviorSubject<User | null>(null);
-  public user = this.userBehaviorSubject.asObservable();
+  private userBehaviorSubject: BehaviorSubject<User | null>;
+  public user;
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {
+    // Load user from localStorage if it exists
+    const savedUser = localStorage.getItem('user') as string;
+    try {
+      let localUser: User = new User();
+      Object.assign(localUser, JSON.parse(savedUser) as Object)
+      this.userBehaviorSubject = new BehaviorSubject<User | null>(localUser);
+    } catch (error) {
+      this.userBehaviorSubject = new BehaviorSubject<User | null>(null);
+      console.log('Error when parsing:', error)
+    }
+    this.user = this.userBehaviorSubject.asObservable();
+  }
 
   async login(username: string, password: string) {
-    await this.http.post(this.backendUrl, { username, password }).subscribe((res: any) => {
+    this.http.post(this.backendUrl, { username, password }).subscribe((res: any) => {
       if (res.message == 'Login successful') {
         this.userBehaviorSubject.next(res.user);
+        localStorage.setItem('user', JSON.stringify(res.user));
+        console.log('User saved:', JSON.stringify(res.user))
         this.router.navigate(['/']);
       }
     });
@@ -28,6 +41,8 @@ export class AuthService {
 
   async logout() {
     this.userBehaviorSubject.next(null);
+    localStorage.removeItem('user');
     this.router.navigate(['/login']);
   }
 }
+
